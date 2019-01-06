91f040af7cfb4d25f341b45afd93843e94275b14
dampo4 <33032031+dampo4@users.noreply.github.com>
2019-01-06 23:46:51 +0000

    fixed powerup mechanic

--- car shooter/Assets/Scripts/NewBehaviourScript.cs
+++ car shooter/Assets/Scripts/NewBehaviourScript.cs
@@ -6,22 +6,52 @@ public class NewBehaviourScript : MonoBehaviour {
     System.Random rnd = new System.Random();
     public GameObject[] types = new GameObject[3];
     private GameObject[] powerups = new GameObject[8];
-    private int[] quadrant = new int[8] { 1, 1, 2, 2, 3, 3, 4, 4 };
+    private int[] quadrant = new int[8] { 0, 1, 2, 3, 4, 5, 6, 7 };
     private Vector3[] pos = new Vector3[8];
-    int xMin = -30;
-    int xMax = 30;
-    int zMin = -30;
-    int zMax = 30;
+    float timer = 0.0f;
+    float xMin = -25f;
+    float xMax = 25f;
+    float zMin = -25f;
+    float zMax = 25f;
 
     // Use this for initialization
     void Start () {
-            GenereateType();
-            GeneratePos();
+        SpawnPowerups();
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        
+        timer += Time.deltaTime;
+        if (timer > 5 )
+        {
+            ClearPowerups();
+            SpawnPowerups();
+            timer = 0;
+        }
+
+    }
+
+    void ClearPowerups()
+    {
+        GameObject[] activePowerups = GameObject.FindGameObjectsWithTag("PowerUp");
+        foreach (GameObject powerup in activePowerups)
+            GameObject.Destroy(powerup);
+    }
+
+    void SpawnPowerups()
+    {
+        GenereateType();
+        GeneratePos();
+        PlaceObjects();
+    }
+
+    void PlaceObjects()
+    {
+        for (int i = 0; i < powerups.Length; i++)
+        {
+            Debug.Log(pos);
+            Instantiate(powerups[i], pos[i], new Quaternion(0, 0, 0, 0));
+        }
 
     }
 
@@ -29,7 +59,7 @@ public class NewBehaviourScript : MonoBehaviour {
     {
         for (int i = 0; i < powerups.Length; i++)
         {
-            powerups[i] = types[rnd.Next(0, types.Length)];
+            powerups[i] = types[Random.Range(0, types.Length)];
         }
     }
 
@@ -37,31 +67,90 @@ public class NewBehaviourScript : MonoBehaviour {
 
     void GeneratePos()
     {
-        foreach (var item in quadrant)
+        for (int i = 0; i < 8; i++)
         {
-            int x = 0;
-            int z = 0;
+            float x = 0;
+            float z = 0;
+            float xMin = -25f;
+            float xMax = 25f;
+            float zMin = -25f;
+            float zMax = 25f;
+            if (i == 0 | i == 1)
+            {
+                do
+                {
+                    x = Random.Range(xMin, -1f);
+                    z = Random.Range(0f, zMax);
+                }
+                while (x < -10 && x > -20 && z > 10 && z < 20 );
+            }
+            else if (i == 2 | i == 3)
+            {
+                do
+                {
+                    x = Random.Range(1f, xMax);
+                    z = Random.Range(0f, zMax);
+                }
+                while (x > 10 && x < 20 && z > 10 && z < 20);
+
+            }
+            else if (i == 4 | i == 5)
+            {
+                do
+                {
+                    x = Random.Range(xMin, -1f);
+                    z = Random.Range(zMin, 0f);
+                }
+                while (x < -10 && x > -20 && z < -10 && z > -20);
+
+            }
+            else if (i == 6 | i == 7)
+            {
+                do
+                {
+                    x = Random.Range(1f, xMax);
+                    z = Random.Range(zMin, 0f);
+                }
+                while (x > 10 && x < 20 && z < -10 && z > -20);
+
+            }
+            pos[i] = new Vector3(x, 0.451f, z);
+        }
+
+
+
+
+
+        /*foreach (int item in quadrant)
+        {
+            float x = 55;
+            float z = 55;
             switch (item)
             {
-                case 1:
-                    x = rnd.Next(-30, 0);
-                    z = rnd.Next(0, 30);
+                case 0 | 4:
+                    x = Random.Range(xMin, 0f);
+                    z = Random.Range(0f, zMax);
+                    pos[item] = new Vector3(x, 0.451f, z);
                     break;
-                case 2:
-                    x = rnd.Next(0, 30);
-                    z = rnd.Next(0, 30);
+                case 1 | 5:
+                    x = Random.Range(0f, xMax);
+                    z = Random.Range(0f, zMax);
+                    pos[item] = new Vector3(x, 0.451f, z);
                     break;
-                case 3:
-                    x = rnd.Next(-30, 0);
-                    z = rnd.Next(-30, 0);
+                case 2 | 6:
+                    x = Random.Range(xMin, 0f);
+                    z = Random.Range(zMin, 0f);
+                    pos[item] = new Vector3(x, 0.451f, z);
                     break;
-                case 4:
-                    x = rnd.Next(0, 30);
-                    z = rnd.Next(-30, 0);
+                case 3 | 7:
+                    x = Random.Range(0f, xMax);
+                    z = Random.Range(zMin, 0f);
+                    pos[item] = new Vector3(x, 0.451f, z);
                     break;
             }
-            pos[item] = new Vector3(x, 0.451f, z);
-        }
+
+        }*/
+
 
     }
 }
